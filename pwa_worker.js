485253cf05a43d125f496aef50e144df9ac686eb
Loren <lorenharris2@gmail.com>
2020-06-14 14:36:19 -0700

    a

--- pwa_worker.js
+++ pwa_worker.js
@@ -28,7 +28,6 @@ self.addEventListener('fetch', event => {
 			var nf = event.request.clone();
 			nf.url = url+"/app/"+nf.url;
 			return fetch(nf).then(function(response) {
-				
 				console.log('Response from network is:', response);
 				
 				cache.put(event.request, response.clone());
